870ff861c430db33a68fd021a3f70592f5af5268
Sanyapushka8 <zimin.sasha.2020@mail.ru>
2025-05-11 10:48:27 +0700

    Add files via upload

--- predict_url_bot.py
+++ predict_url_bot.py
@@ -1,9 +1,15 @@
 import asyncio
 import logging
 import pickle
+
+from aiogram.dispatcher.router import message
+from aiogram.dispatcher import router
+from aiogram.types import Message
 from keras.models import load_model
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from telebot.states import StatesGroup, State
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -11,6 +17,13 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token="7930769291:AAEiCjYWaSMu6xqWGdtgi_JGa0NhlMbi1pU")
 # Диспетчер
 dp = Dispatcher()
+
+#Загружаем готовый материал
+MODEL = load_model('url_classifier_LSTM.keras')
+with open('tokenizer.pkl', 'rb') as f:
+    TOKENIZER = pickle.load(f)
+with open('LabelEncoder.pkl', 'rb') as f:
+      LE = pickle.load(f)
 
 from datetime import datetime
 dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -20,21 +33,31 @@ async def cmd_info(message: types.Message, started_at: str):
     await message.answer(f"Бот запущен {started_at}")
 
 # Хэндлер на команду /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Напиши мне URL и я проверю её на вредоность")
+#@dp.message(Command("start"))
+#async def cmd_start(message: types.Message):
+#    await message.answer("Напиши мне URL и я проверю её на вредоность")
 
 @dp.message(Command("dice"))
 async def cmd_dice(message: types.Message):
     await message.answer_dice(emoji="🎲")
 
-#Загружаем готовый материал
-MODEL = load_model('url_classifier_LSTM.keras')
-with open('tokenizer.pkl', 'rb') as f:
-    TOKENIZER = pickle.load(f)
-with open('LabelEncoder.pkl', 'rb') as f:
-      LE = pickle.load(f)
+class URLs(StatesGroup):
+     url_name = State()
+     url_classifier = State()
 
+class FNSContext:
+    pass
+
+
+@router.message(StateFilter(None), Command('start'))
+async def cmd_ctart(message: Message, state: FNSContext):
+    await message.answer(
+        text='Напишите URL🤩'
+        )
+    await state.set_state(URLs.url_name)
+
+@router.message(URLs.url_name)
+async def classifier(message: Message, state: FNSContext):
 
 
 
@@ -45,6 +68,11 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
+# импорты
 
+
+# Для записей с типом Secret* необходимо
+# вызывать метод get_secret_value(),
+# чтобы получить настоящее содержимое вместо '*******'
 
 
